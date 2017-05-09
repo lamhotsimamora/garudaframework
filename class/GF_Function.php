af8149bfce085dbba2e606846ed0d7ad0914dd52
Lamhot Simamora <lamhotsimamora36@gmail.com>
2017-05-10 01:36:57 +0700

    Update GF_Function.php

--- class/GF_Function.php
+++ class/GF_Function.php
@@ -199,3 +199,19 @@ public     function getIconFile($val){
     }     
 
 }
+function compress_image($source_url, $destination_url, $quality) {
+
+    $info = getimagesize($source_url);
+
+    if ($info['mime'] == 'image/jpeg')
+    $image = imagecreatefromjpeg($source_url);
+
+    elseif ($info['mime'] == 'image/gif')
+    $image = imagecreatefromgif($source_url);
+
+    elseif ($info['mime'] == 'image/png')
+    $image = imagecreatefrompng($source_url);
+
+    imagejpeg($image, $destination_url, $quality);
+    return true;
+}  
